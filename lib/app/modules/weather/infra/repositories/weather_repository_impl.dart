8c55c825c0755a6d3240c6c42ff2d90f35c89666
Marco Fernandes <marcoaferc@gmail.com>
2023-11-01 19:06:55 -0300

    Create weather infra

---
+++ lib/app/modules/weather/infra/repositories/weather_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:skywatch/app/core/errors/failure.dart';
+import 'package:skywatch/app/modules/weather/domain/entities/forecast.dart';
+import 'package:skywatch/app/modules/weather/domain/entities/weather.dart';
+import 'package:skywatch/app/modules/weather/domain/repositories/weather_repository.dart';
+import 'package:skywatch/app/modules/weather/infra/datasources/weather_datasource.dart';
+
+class WeatherRepositoryImpl implements WeatherRepository {
+  final WeatherDatasource datasource;
+
+  WeatherRepositoryImpl({required this.datasource});
+
+  @override
+  Future<List<Forecast>> getForecast(
+      {required String lat, required String lon}) async {
+    return await datasource.getForecast(lat: lat, lon: lon);
+  }
+
+  @override
+  Future<Weather> getWeather({required String lat, required String lon}) async {
+    try {
+      return await datasource.getWeather(lat: lat, lon: lon);
+    } on Failure {
+      rethrow;
+    } catch (error, stackTrace) {
+      throw Failure(
+          message: 'An unexpected error has ocurred, please try again later',
+          stackTrace: stackTrace);
+    }
+  }
+}
